9564ddbf3641010fcc0940ee78fd3c189d0f479c
Никита Ангелов <angeloff555@mail.ru>
2023-02-14 18:52:31 +0300

    update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,10 +1,13 @@
+import random
+
+
 RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 MIN = 1
 MAX = 100
 
 
 def get_result():
-    question = randint(MIN, MAX)
+    question = random.randint(MIN, MAX)
 
     if question % 2 == 0:
         answer = 'yes'
