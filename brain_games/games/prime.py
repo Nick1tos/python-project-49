2df8d2dd58994fe2fffaf7ea2bffa7f17d8d5619
Никита Ангелов <angeloff555@mail.ru>
2023-02-25 15:59:48 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,11 +7,11 @@ MAX = 50
 
 def get_result():
     question = random.randint(MIN, MAX)
+    return question
+
+
+def prime_result(question):
     answer = 'yes'
-    return question, answer
-
-
-def prime_result(question, answer):
     for i in range(2, int(question / 2) + 1):
         if (question % i) == 0:
             answer = 'no'
